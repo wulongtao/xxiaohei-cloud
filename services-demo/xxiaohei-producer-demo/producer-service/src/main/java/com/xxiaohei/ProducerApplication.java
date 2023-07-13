68cd12041bc0540bd8c771d763d3e818ba66f2dd
wulongtao <wulongtao@elimen.com.cn>
2023-07-13 22:32:11 +0800

    [core] 框架搭建

---
+++ services-demo/xxiaohei-producer-demo/producer-service/src/main/java/com/xxiaohei/ProducerApplication.java
@@ -0,0 +1,16 @@
+package com.xxiaohei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 小小黑
+ */
+@SpringBootApplication
+public class ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
+}
---
+++ services-demo/xxiaohei-producer-demo/producer-service/src/main/java/com/xxiaohei/nacos/NacosDiscoveryConfiguration.java
@@ -0,0 +1,14 @@
+package com.xxiaohei.nacos;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 小小黑
+ */
+@EnableDiscoveryClient
+@Configuration
+public class NacosDiscoveryConfiguration {
+
+
+}
