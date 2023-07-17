d518c424a4244040e427274669a574c39bc26e85
wulongtao <769418278@qq.com>
2023-07-17 21:29:08 +0800

    [core] 框架搭建

--- services-demo/xxiaohei-consumer-demo/consumer-service/src/main/java/com/xxiaohei/ConsumerApplication.java
+++ services-demo/xxiaohei-consumer-demo/consumer-service/src/main/java/com/xxiaohei/ConsumerApplication.java
@@ -5,10 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * @author Administrator
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 public class ConsumerApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
